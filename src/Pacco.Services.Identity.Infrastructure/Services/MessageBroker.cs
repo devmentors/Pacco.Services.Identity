c3155b7d59c8fd529f8a97621a7c9162b2eca7c1
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-12-09 16:10:35 +0100

    Updated MessageBroker

--- src/Pacco.Services.Identity.Infrastructure/Services/MessageBroker.cs
+++ src/Pacco.Services.Identity.Infrastructure/Services/MessageBroker.cs
@@ -53,6 +53,7 @@ namespace Pacco.Services.Identity.Infrastructure.Services
 
             var messageProperties = _messagePropertiesAccessor.MessageProperties;
             var correlationId = messageProperties?.CorrelationId;
+            var userId = messageProperties?.UserId;
             var spanContext = messageProperties?.GetSpanContext(_spanContextHeader);
             if (string.IsNullOrWhiteSpace(spanContext))
             {
@@ -74,12 +75,13 @@ namespace Pacco.Services.Identity.Infrastructure.Services
                 _logger.LogTrace($"Publishing integration event: {@event.GetType().Name} [id: '{messageId}'].");
                 if (_outbox.Enabled)
                 {
-                    await _outbox.SendAsync(@event, messageId, correlationId, spanContext, correlationContext, headers);
+                    await _outbox.SendAsync(@event, messageId, correlationId, spanContext, correlationContext, headers,
+                        userId);
                     continue;
                 }
 
                 await _busPublisher.PublishAsync(@event, messageId, correlationId, spanContext, correlationContext,
-                    headers);
+                    headers, userId);
             }
         }
     }
